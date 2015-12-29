36a9d5cd79c8e95a38a6c9e26ed62e6ca538048b
Thomas W <thomas@matchstrike.net>
2015-12-29 15:54:42 -0800

    An auspicious start.

---
+++ scripts/build.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd "`dirname $0`/.."
+
+set -e
+
+VERSION=`node -e "console.log(require('./package.json').version);"`;
+NAME=`node -e "console.log(require('./package.json').productName);"`;
+
+echo "Building $NAME $VERSION"
+
+node_modules/.bin/electron-packager . $NAME \
+--platform=darwin \
+--arch=x64 \
+--version=0.36.2 \
+--icon=src/img/icon.icns \
+--out=build \
+--app-version=$VERSION \
+--overwrite;
+
+open build/HangInThere-darwin-x64/HangInThere.app;
---
+++ src/main.js
@@ -0,0 +1,40 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function() {
+  mainWindow = new BrowserWindow({
+    minWidth: 1024,
+    minHeight: 768,
+    width: 1500,
+    height: 768,
+    //transparent: true,
+    title: 'HangInThere',
+    icon: 'file://'+ __dirname + '/img/icon.png'
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+});
