3f48369f71d706c5eba6e944e162f9e1e1e81ab7
Thomas W <thomas@matchstrike.net>
2016-01-01 23:47:55 -0800

    Make menu actions fire events
    This moves the implementation details about what happens when a user clicks a menu item out of the menus module and into main.js.

--- src/main.js
+++ src/main.js
@@ -8,16 +8,42 @@ const WindowManager = require('./windows.js');
 const MenuBuilder = require('./menus.js');
 
 let windowManager = new WindowManager({app: app});
-let menuBuilder = new MenuBuilder({menu: electron.Menu})
+let menuBuilder = new MenuBuilder({
+  appName: app.getName(),
+  Menu: electron.Menu
+});
+
+function reload_window(menuItem, activeWindow) {
+  if (activeWindow) {
+    activeWindow.reload();
+  }
+}
+
+function full_screen_window(menuItem, activeWindow) {
+  if (activeWindow) {
+    activeWindow.setFullScreen(!activeWindow.isFullScreen());
+  }
+}
+
+function toggle_dev_tools_for_window(menuItem, activeWindow) {
+  if (activeWindow) {
+    activeWindow.toggleDevTools();
+  }
+}
+
+function initialize_menu() {
+  electron.Menu.setApplicationMenu(menuBuilder.menu);
+}
 
 function initialize_hangouts_window() {
   windowManager.addWindow(new HangoutsWindow());
 }
 
-function initialize_menu() {
-  var menu = menuBuilder.build_menu();
-  electron.Menu.setApplicationMenu(menu);
-}
+menuBuilder.on('new_window-clicked', initialize_hangouts_window);
+menuBuilder.on('quit-clicked', app.quit);
+menuBuilder.on('reload-clicked', reload_window);
+menuBuilder.on('full_screen-clicked', full_screen_window);
+menuBuilder.on('dev_tools-clicked', toggle_dev_tools_for_window);
 
 app.on('ready', initialize_hangouts_window);
 app.on('ready', initialize_menu);
--- src/menus.js
+++ src/menus.js
@@ -1,189 +1,204 @@
 'use strict';
 
-const electron = require('electron');
+const EventEmitter = require('events').EventEmitter;
+const util = require('util');
 const _ = require('lodash');
 
-var appName = electron.app.getName();
-
-var fileMenu = {
-  label: 'File',
-  submenu: [
-    {
-      label: 'New chat window...',
-      accelerator: 'CmdOrCtrl+N',
-      click: function() { console.log("Attempted to open new window...."); }
-    },
-    {
-      label: 'About ' + appName,
-      role: 'about'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Quit',
-      accelerator: 'CommandOrCtrl+Q',
-      click: function() { electron.app.quit(); }
-    },
-  ]
-}
-
-var editMenu = {
-  label: 'Edit',
-  submenu: [
-    {
-      label: 'Undo',
-      accelerator: 'CmdOrCtrl+Z',
-      role: 'undo'
-    },
-    {
-      label: 'Redo',
-      accelerator: 'Shift+CmdOrCtrl+Z',
-      role: 'redo'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Cut',
-      accelerator: 'CmdOrCtrl+X',
-      role: 'cut'
-    },
-    {
-      label: 'Copy',
-      accelerator: 'CmdOrCtrl+C',
-      role: 'copy'
-    },
-    {
-      label: 'Paste',
-      accelerator: 'CmdOrCtrl+V',
-      role: 'paste'
-    },
-    {
-      label: 'Select All',
-      accelerator: 'CmdOrCtrl+A',
-      role: 'selectall'
-    },
-  ]
-}
-
-var viewMenu = {
-  label: 'View',
-  submenu: [
-    {
-      label: 'Reload',
-      accelerator: 'CmdOrCtrl+R',
-      click: function(item, focusedWindow) {
-        if (focusedWindow)
-          focusedWindow.reload();
-      }
-    },
-    {
-      label: 'Toggle Full Screen',
-      accelerator: process.platform == 'darwin' ? 'Ctrl+Command+F' : 'F11',
-      click: function(item, focusedWindow) {
-        if (focusedWindow) {
-          focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
-        }
-      }
-    },
-    {
-      label: 'Toggle Developer Tools',
-      accelerator: process.platform == 'darwin' ? 'Alt+Command+I' : 'Ctrl+Shift+I',
-      click: function(item, focusedWindow) {
-        if (focusedWindow) {
-          focusedWindow.toggleDevTools();
-        }
-      }
-    },
-  ]
-}
-
-var windowMenu = {
-  label: 'Window',
-  role: 'window',
-  submenu: [
-    {
-      label: 'Minimize',
-      accelerator: 'CmdOrCtrl+M',
-      role: 'minimize'
-    },
-    {
-      label: 'Close',
-      accelerator: 'CmdOrCtrl+W',
-      role: 'close'
-    },
-  ]
-}
-
-var darwinAppMenu = {
-  label: appName,
-  submenu: [
-    {
-      label: 'New chat window...',
-      accelerator: 'CmdOrCtrl+N',
-      click: function() { console.log("Attempted to open new window...."); }
-    },
-    {
-      label: 'About ' + appName,
-      role: 'about'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Hide ' + appName,
-      accelerator: 'Command+H',
-      role: 'hide'
-    },
-    {
-      label: 'Hide Others',
-      accelerator: 'Command+Shift+H',
-      role: 'hideothers'
-    },
-    {
-      label: 'Show All',
-      role: 'unhide'
-    },
-    {
-      type: 'separator'
-    },
-    {
-      label: 'Quit',
-      accelerator: 'Command+Q',
-      click: function() { electron.app.quit(); }
-    },
-  ]
-}
-
-var darwinWindowMenu = _.cloneDeep(windowMenu);
-darwinWindowMenu.submenu.push([
-  {
-    type: 'separator'
-  },
-  {
-    label: 'Bring All to Front',
-    role: 'front'
-  }
-]);
-
-var template = [fileMenu, editMenu, viewMenu, windowMenu];
-
-if (process.platform === 'darwin') {
-  var template = [darwinAppMenu, editMenu, viewMenu, darwinWindowMenu]
-}
 
 class MenuBuilder {
   constructor(settings) {
-    this.template = template;
-    this.menu = settings.menu;
+    this.appName = settings.appName;
+    this.Menu = settings.Menu;
+    this.isDarwin = process.platform === 'darwin';
+
+    this.fileMenu = {
+      label: 'File',
+      submenu: [
+        {
+          label: 'New chat window...',
+          accelerator: 'CmdOrCtrl+N',
+          id: 'new_window'
+        },
+        {
+          label: 'About ' + this.appName,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'CommandOrCtrl+Q',
+          id: 'quit'
+        }
+      ]
+    }
+
+    this.editMenu = {
+      label: 'Edit',
+      submenu: [
+        {
+          label: 'Undo',
+          accelerator: 'CmdOrCtrl+Z',
+          role: 'undo'
+        },
+        {
+          label: 'Redo',
+          accelerator: 'Shift+CmdOrCtrl+Z',
+          role: 'redo'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Cut',
+          accelerator: 'CmdOrCtrl+X',
+          role: 'cut'
+        },
+        {
+          label: 'Copy',
+          accelerator: 'CmdOrCtrl+C',
+          role: 'copy'
+        },
+        {
+          label: 'Paste',
+          accelerator: 'CmdOrCtrl+V',
+          role: 'paste'
+        },
+        {
+          label: 'Select All',
+          accelerator: 'CmdOrCtrl+A',
+          role: 'selectall'
+        }
+      ]
+    }
+
+    this.viewMenu = {
+      label: 'View',
+      submenu: [
+        {
+          label: 'Reload',
+          accelerator: 'CmdOrCtrl+R',
+          id: 'reload'
+        },
+        {
+          label: 'Toggle Full Screen',
+          accelerator: this.isDarwin ? 'Ctrl+Command+F' : 'F11',
+          id: 'full_screen'
+        },
+        {
+          label: 'Toggle Developer Tools',
+          accelerator: this.isDarwin ? 'Alt+Command+I' : 'Ctrl+Shift+I',
+          id: 'dev_tools'
+        }
+      ]
+    }
+
+    this.windowMenu = {
+      label: 'Window',
+      role: 'window',
+      submenu: [
+        {
+          label: 'Minimize',
+          accelerator: 'CmdOrCtrl+M',
+          role: 'minimize'
+        },
+        {
+          label: 'Close',
+          accelerator: 'CmdOrCtrl+W',
+          role: 'close'
+        }
+      ]
+    }
+
+    this.darwinAppMenu = {
+      label: this.appName,
+      submenu: [
+        {
+          label: 'New chat window...',
+          accelerator: 'CmdOrCtrl+N',
+          id: 'new_window'
+        },
+        {
+          label: 'About ' + this.appName,
+          role: 'about'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Hide ' + this.appName,
+          accelerator: 'Command+H',
+          role: 'hide'
+        },
+        {
+          label: 'Hide Others',
+          accelerator: 'Command+Shift+H',
+          role: 'hideothers'
+        },
+        {
+          label: 'Show All',
+          role: 'unhide'
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          id: 'quit'
+        }
+      ]
+    }
+
+    this.darwinWindowMenu = _.cloneDeep(this.windowMenu);
+    this.darwinWindowMenu.submenu.push(
+      {
+        type: 'separator'
+      },
+      {
+        label: 'Bring All to Front',
+        role: 'front'
+      }
+    );
+
+    this.template = this.get_template();
+    this.menu = this.Menu.buildFromTemplate(this.template);
   }
 
-  build_menu() {
-    console.log("Menu template:", this.menu.buildFromTemplate(this.template));
-    return this.menu.buildFromTemplate(this.template);
+  get_template() {
+    var template = [
+      this.fileMenu, this.editMenu, this.viewMenu, this.windowMenu];
+
+    if (this.isDarwin) {
+      template = [
+        this.darwinAppMenu, this.editMenu, this.viewMenu,
+        this.darwinWindowMenu];
+    }
+
+    this.bind_click_handler(template);
+
+    return template;
   }
 
+  bind_click_handler(template) {
+    for (var menuItem of template) {
+      for (var subMenuItem of menuItem.submenu) {
+        subMenuItem.click = this.menu_item_clicked.bind(this);
+      }
+    }
+  }
+
+  menu_item_clicked(menuItem, activeWindow) {
+    console.log('"' + menuItem.label + '" menu item invoked.');
+    this.emit('menu-item-clicked', menuItem, activeWindow);
+
+    if (menuItem.id) {
+      this.emit(menuItem.id + '-clicked', menuItem, activeWindow);
+    }
+  }
 }
+
+util.inherits(MenuBuilder, EventEmitter);
 
 module.exports = MenuBuilder;
