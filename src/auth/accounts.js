a822dbcf242228d25e636cf5cf76af21cf7e6929
Thomas W <thomas@matchstrike.net>
2016-01-13 22:48:27 -0800

    AuthManager promise cleanup

--- src/auth/accounts.js
+++ src/auth/accounts.js
@@ -22,22 +22,37 @@ class AccountManager {
   _getAccounts(resolve, reject) {
     var browser = this._getBrowser();
 
+    var resolveAndCleanup = (value) => {
+      resolve(value);
+      browser.destroy();
+    };
+
+    var rejectAndCleanup = (reason) => {
+      reject(reason);
+      browser.destroy();
+    };
+
     browser.loadURL(this._url);
 
     browser.once('account-data-loaded', (data, error) => {
       if (error) {
-        reject();
+        rejectAndCleanup(error);
       }
 
-      resolve(data);
-
-      if (this._invisible) {
-        browser.destroy();
-      }
+      resolveAndCleanup(data);
     });
 
-    browser.webContents.once('did-fail-load', reject);
-    browser.webContents.once('crashed', reject);
+    setTimeout(() => {
+      rejectAndCleanup(new Error('Timeout after 10000 ms'));
+    }, 10000);
+
+    browser.webContents.once('did-fail-load', () => {
+      rejectAndCleanup(new Error('Failed to load account data.'));
+    });
+
+    browser.webContents.once('crashed', () => {
+      rejectAndCleanup(new Error('Failed to load account data.'));
+    });
   }
 
   getAccounts() {
