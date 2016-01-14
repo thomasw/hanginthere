2ea4a94d2bccf506097d03426893fbae14d03195
Thomas W <thomas@matchstrike.net>
2016-01-13 23:57:18 -0800

    Retrieve account data
    Retrieve account data, and if no users are logged in, show a login dialog.

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ const app = electron.app;
 const ipcMain = electron.ipcMain;
 const session = electron.session;
 
+const AccountManager = require('./auth/accounts');
+const LoginWindow = require('./auth/login-window');
 const WindowManager = require('./window-manager');
 const DockNotifier = require('./dock-notifier');
 
@@ -11,6 +13,7 @@ const MenuBuilder = require('./menus');
 const bindWindowEvents = require('./window-events');
 
 
+let accountManager = new AccountManager();
 let windowManager = new WindowManager();
 let dockNotifier = new DockNotifier({
   dock: app.dock,
@@ -20,6 +23,7 @@ let menuBuilder = new MenuBuilder({
   appName: app.getName(),
   Menu: electron.Menu
 });
+let accounts = [];
 
 function reloadWindow(menuItem, activeWindow) {
   activeWindow && activeWindow.reload();
@@ -39,6 +43,21 @@ function clearSessionDataAndQuit() {
   });
 }
 
+function init() {
+  accountManager.getAccounts().then((data) => {
+    console.log('Authenticated accounts:', data);
+
+    accounts = data;
+  }).then(() => {
+    if (accounts.length === 0) {
+      new LoginWindow();
+    }
+  }).catch((error) => {
+    console.log('Unable to retrieve account data.', error);
+    app.quit();
+  });
+}
+
 menuBuilder.on('quit-clicked', app.quit);
 menuBuilder.on('reload-clicked', reloadWindow);
 menuBuilder.on('full_screen-clicked', fullScreenWindow);
@@ -48,10 +67,13 @@ menuBuilder.on('next_window-clicked',
 menuBuilder.on('log_out-clicked', clearSessionDataAndQuit);
 
 app.on('ready', () => { electron.Menu.setApplicationMenu(menuBuilder.menu); });
+app.on('ready', init);
+app.on('window-all-closed', () => {}); // Prevent the default (app closes)
 
 app.on('browser-window-created', (e, win) => { windowManager.addWindow(win); });
 app.on('browser-window-created', (e, win) => { bindWindowEvents(win); });
 
 app.on('browser-window-focus', dockNotifier.resetDock.bind(dockNotifier));
 app.on('activate', dockNotifier.resetDock.bind(dockNotifier));
+
 ipcMain.on('message-received', dockNotifier.messageReceived.bind(dockNotifier));
