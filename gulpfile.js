31c7e5422138de9374d8dd706633aecc5df65542
Thomas W <thomas@matchstrike.net>
2016-02-07 16:03:37 -0800

    build: Keep invalid js syntax from killing watch

--- gulpfile.js
+++ gulpfile.js
@@ -3,38 +3,45 @@ const browserify = require('browserify');
 const vbuffer = require('vinyl-buffer');
 const del = require('del');
 const gulp = require('gulp');
+const gutil = require('gulp-util');
 const runSequence = require('run-sequence');
 const source = require('vinyl-source-stream');
 const sourcemaps = require('gulp-sourcemaps');
 
-const babelOutDir = 'src/chat/dist';
+const jsOutDir = 'src/chat/dist';
 
-gulp.task('clean', (cb) => {
-  del(babelOutDir).then(() => {
-    cb();
-  });
+const handleError = function(err) {
+  gutil.log(
+      gutil.colors.red('Browserify compile error:'),
+      err.message
+  );
+  gutil.beep(3);
+  this.emit('end');
+};
+
+gulp.task('clean', () => {
+  return del(jsOutDir);
 });
 
-gulp.task('build', ['clean'], (cb) => {
+gulp.task('buildJS', ['clean'], (cb) => {
   runSequence(['babel-build'], cb);
 });
 
 gulp.task('babel-build', () => {
-    return browserify('./src/chat/js/index.js', {
-      debug: true
-    })
-      .transform(babelify)
-      .bundle()
-      .on('error', (err) => { console.error(err); this.emit('end'); })
-      .pipe(source('build.js'))
-      .pipe(vbuffer())
-      .pipe(sourcemaps.init({ loadMaps: true }))
-      .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest(babelOutDir));
+  return browserify('./src/chat/js/index.js', {
+    debug: true
+  }).transform(babelify)
+    .bundle()
+    .on('error', handleError)
+    .pipe(source('build.js'))
+    .pipe(vbuffer())
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest(jsOutDir));
 });
 
 gulp.task('watch', function() {
-    return gulp.watch(['src/chat/js/**/*'], ['build']);
+    return gulp.watch(['src/chat/js/**/*'], ['buildJS']);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('default', ['buildJS', 'watch']);
