768f4088fc46cfaac58292902baeff3955fe92e1
Thomas W <thomas@matchstrike.net>
2016-02-07 21:46:50 -0800

    Allow unread counts and conatct data in state

--- src/chat/js/actions.js
+++ src/chat/js/actions.js
@@ -1,8 +1,14 @@
 export const UPDATE_ACCOUNTS = 'UPDATE_ACCOUNTS'
 export const ACTIVATE_ACCOUNT = 'ACTIVATE_ACCOUNT'
 export const SET_LOADING = 'SET_LOADING'
+export const CONTACT_UPDATE = 'CONTACT_UPDATE'
+export const SET_UNREAD_COUNTS = 'SET_UNREAD_COUNTS'
 
 export function updateAccounts (accounts) {
+  accounts = accounts.map((account) => {
+    account.unreadCount = 0
+    return account
+  })
   return { type: UPDATE_ACCOUNTS, accounts }
 }
 
@@ -12,4 +18,12 @@ export function activateAccount(account) {
 
 export function setLoading() {
   return { type: SET_LOADING }
+}
+
+export function updateContact(contact) {
+  return { type: CONTACT_UPDATE, contact }
+}
+
+export function setUnreadCounts(unreadCounts) {
+  return { type: SET_UNREAD_COUNTS, unreadCounts}
 }
--- src/chat/js/reducers.js
+++ src/chat/js/reducers.js
@@ -1,5 +1,8 @@
 import { combineReducers } from 'redux'
-import { UPDATE_ACCOUNTS, ACTIVATE_ACCOUNT, SET_LOADING } from './actions'
+import {
+  UPDATE_ACCOUNTS, ACTIVATE_ACCOUNT, SET_LOADING, CONTACT_UPDATE,
+  SET_UNREAD_COUNTS
+} from './actions'
 
 function accounts(state = [], action) {
   switch (action.type) {
@@ -11,6 +14,12 @@ function accounts(state = [], action) {
       // Google always adds new accounts to the bottm, so we can get always
       // with ignoring however many items are already in the accounts list.
       return [].concat(state, action.accounts.slice(state.length))
+    case SET_UNREAD_COUNTS:
+      return state.map((account, idx) => {
+        return Object.assign({}, account, {
+          unreadCount: action.unreadCounts[idx]
+        })
+      })
     default:
       return state
   }
@@ -27,7 +36,21 @@ function selectedAccount(state = null, action) {
   }
 }
 
+function contacts(state = {}, action) {
+  switch(action.type) {
+    case CONTACT_UPDATE:
+      let contactUpdate = {}
+      contactUpdate[action.contact.id] = action.contact;
+
+      return Object.assign({}, state, contactUpdate)
+    default:
+      return state
+  }
+
+}
+
 export default combineReducers({
   'accounts': accounts,
+  'contacts': contacts,
   'selectedAccount': selectedAccount
 })
