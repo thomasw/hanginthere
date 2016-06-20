2824dc53113e73fb8c35fbb5bdc4aa8cca62b70d
Thomas Welfley <thomas.welfley@gmail.com>
2016-06-20 14:45:41 -0700

    Handle URL clicks coming from chat windows
    If a clicked URL isn't a hangout URL, we send it to the system browser. If it's a hangout URL, we open a new HangInThere window to handle the video chat.

--- src/chat/js/components/Chat.js
+++ src/chat/js/components/Chat.js
@@ -1,8 +1,17 @@
 import React, { Component, PropTypes } from 'react'
-import ReactDOM from 'react-dom';
-import path from 'path';
+import ReactDOM from 'react-dom'
+import path from 'path'
 
 /*global fs:false, chat_dir:false */
+
+const videoChatURLs = [
+  'https://talkgadget.google.com/', 'https://hangouts.google.com/']
+
+function isVideoChatUrl(url) {
+  return videoChatURLs.some((x) => {
+    return url.startsWith(x);
+  });
+}
 
 export default class Chat extends Component {
 
@@ -12,6 +21,8 @@ export default class Chat extends Component {
     webview.addEventListener('did-get-response-details', this.injectCss)
     webview.addEventListener('ipc-message', this.handleContactUpdate.bind(this))
     webview.addEventListener('console-message', this.logMessage)
+    webview.addEventListener('will-navigate', this.handleNavigation.bind(this))
+    webview.addEventListener('new-window', this.handleNavigation.bind(this))
   }
 
   componentWillUnmount() {
@@ -20,6 +31,8 @@ export default class Chat extends Component {
     webview.removeEventListener('did-get-response-details', this.injectCss)
     webview.removeEventListener('console-message', this.logMessage)
     webview.removeEventListener('ipc-message', this.handleContactUpdate)
+    webview.removeEventListener('will-navigate', this.handleNavigation)
+    webview.removeEventListener('new-window', this.handleNavigation)
   }
 
   handleContactUpdate(e) {
@@ -31,6 +44,26 @@ export default class Chat extends Component {
     contact.account = this.props.id
 
     this.props.onContactUpdate(contact);
+  }
+
+  handleNavigation(e) {
+    const url = e.url
+
+    console.log('BrowserWindow new window invokation attempt:', e, url, this)
+    e.preventDefault()
+
+    if(isVideoChatUrl(url)) {
+      this.openWindow(url)
+      return false
+    }
+
+    electron.shell.openExternal(url)
+    return false
+  }
+
+  openWindow(url) {
+    let win = new electron.remote.BrowserWindow({width: 800, height: 600})
+    win.loadURL(url)
   }
 
   injectCss(e) {
